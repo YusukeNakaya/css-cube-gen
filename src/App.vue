4359dd35ccf3805ffcc5117e84bd957a87c0f217
YusukeNakaya <yusuke.nakaya@oro.com>
2021-04-27 21:56:04 +0900

    :tada:

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CubeGen msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CubeGen from './components/CubeGen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CubeGen
   }
 }
 </script>
